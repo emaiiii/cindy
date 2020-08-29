b987908bbd17ae7d4a763acd2565266f9dcee035
emaiiii <emai003@ucr.edu>
2020-08-29 14:47:25 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
-import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
 import './App.css';
 
 import letter from './images/letter.jpg';
@@ -16,83 +12,13 @@ import image4 from './images/image4.jpg';
 import image5 from './images/image5.jpg';
 import image7 from './images/image7.jpg';
 
-
-// api key
-const app = new Clarifai.App({
- apiKey: 'a0a70926e040489eafbb16b5509c97a8'
-});
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '',
-      box:{},
       route:'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
+      isSignedIn: false
     }
-  }
-
-  loadUser = (data) => {
-    this.setState({user: {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joined
-    }})
-  }
-
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    console.log(width, height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
-
-  displayFaceBox = (box) => {
-    this.setState({box:box});
-  }
-
-  onInputChange = (event) => {
-    this.setState({input:event.target.value});
-  }
-
-  onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => {
-      if(response) {
-        fetch('http://localhost:3000/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
-         })
-        })
-          .then(response => response.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-      }
-      this.displayFaceBox(this.calculateFaceLocation(response))
-    })
-    .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
@@ -102,19 +28,23 @@ class App extends Component {
     else if(route === 'home'){
       this.setState({isSignedIn: true});
     }
+    else if(route === 'note'){
+      this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
 
   render(){
 
-    const {isSignedIn, imageUrl, route, box} = this.state;
+    const {isSignedIn, route} = this.state;
 
     return (
       <div className="App">
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        <Navigation route={route} isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
         { route === 'home' 
           ? <div>
               <Logo/>
+              <h1>Happy Anniversary to the Most Special Person</h1>
               <div class="container">
                 <img src={image1} alt="image1"/>
                 <img src={image2} alt="image2"/>
@@ -123,17 +53,54 @@ class App extends Component {
                 <img src={image5} alt="image5"/>
                 <img src={image7} alt="image7"/>
              </div>
-             <h1>You are the love of my life</h1>
+             <h1 text-col>❤️ You are the True Love of My Life ❤️</h1>
+             <h1 align="left">Picture 1: This was one of the most important days of my life. This is because on this day, I asked you to be my girlfriend. 
+             It was one small moment that led to so much happiness for me.</h1>
+             <h1 align="left">Picture 2: On this day, I was so happy, but I was also so scared. I was happy, because we had graduated together, and you 
+             were still in my life. I was scared, because we would be going off to college, not knowing what could happen.</h1>
+             <h1 align="left">Picture 3: This is my favorite picture from our first trip to San Francisco together. On this trip, I remember thinking about
+             how much I love you. One reason I love you so much is because, you bring more joy to my life than I can ask for.</h1>
+             <h1 align="left">Picture 4: This picture is of us on our second trip to San Francisco together. It was even more fun than the first, because
+             it was just with you. You and only you alone can make me feel happiness that people spend their lives searching for.</h1>
+             <h1 align="left">Picture 5: I just liked this picture of us, so I added it.LOL. I also look pretty good don't you think? Not too fat for once.</h1>
+             <h1 align="left">Picture 6: As we move on to our next chapter in life, I know that you are thinking, or have thought, about what will become 
+             of us. I have faith in us, and I believe we have been through so much to let anything ever destroy us. There is no possibility of me 
+             not loving you. My love will follow you for eternity.🌟</h1>
             </div>
           : (
               route === 'signin'
               ? <div>
-                  <Signin className="right"loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+                  <Signin className="right" onRouteChange={this.onRouteChange}/>
                   <div className="tilt br3 ba dark-gray b--black-10 mv6 w-100 w-50-m w-50-l mw7 shadow-5 left">
                     <img src={letter} alt="letter"/>
                   </div>
                 </div>
-              : <h1>You not my girlfriend</h1>
+              : <div>
+                  <Logo/>
+                  <div className="white-box">
+                    <p align="left">
+                    Dear Cindy Phan, <br></br><br></br>
+                    
+                    I know that sometimes you question if I love you or not. I don't blame you, because sometimes I am extremely selfish and
+                    inconsiderate. Sometimes I don't do right by you, and I don't know what comes over me to do those selfish things. I am truly
+                    sorry for all the pain I've caused you over the past year, and I want to try to make it right. I admit that I have changed during 
+                    the time from highschool to now. My ways of expressing emotions are totally different now. I want to try and relearn that way
+                    of expression, but I need you to be patient with me and help me. I want to do this because, I feel like I was just an overall better person
+                    years ago when I knew how to convey my feelings. Most importantly though, I want to do this in order to bring you to the level of happiness 
+                    that you experienced early in our relationship. Nothing makes me happier than to see you happy. I promise you with my life, that I will do my 
+                    best to do all of that. Next, I would like to thank you for everything you have done for me. My life would empty without you. You are my guiding 
+                    light that pushes me to be a better me. Not only do you push me to do better, you instill happiness in my life like no other. There is no one
+                    else that I would rather go on crazy boba and food runs with. There is no one else that I would rather have dance battles with. There is no one
+                    else that I would rather spend the rest of my life with. All the peak happy moments in my life have been because of you. You have this aura about 
+                    you that is indescribable. You just make everything around you simply—better. That is why I look up to you. I aspire to be a great person like you.
+
+                    <br></br><br></br>
+                    Happy 6 Year Anniversary from yours,
+                    <br></br><br></br>
+                    Erik Mai ❤️
+                    </p>
+                  </div>
+                </div>
             )
         }
       </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const Navigation = ({onRouteChange, isSignedIn}) => {
-	if(isSignedIn) {
+const Navigation = ({route, onRouteChange, isSignedIn}) => {
+	if(isSignedIn && route === 'home') {
 		return(
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p onClick={() => onRouteChange('note')} className='f3 link dim black underline pa3 pointer'>Note</p>
 				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
 			</nav>
 		)
-	} else{
+	} 
+	else if(isSignedIn && route === 'note'){
+		return(
+			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p onClick={() => onRouteChange('home')} className='f3 link dim black underline pa3 pointer'>Home</p>
+				<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+			</nav>
+		)
+	}
+	else{
 		return(
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			</nav>
