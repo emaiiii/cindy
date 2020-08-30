082c835c9a4833aa5362f22ab2d814c2f557cae9
emaiiii <airwickmai@gmail.com>
2020-08-30 00:22:44 -0700

    Updates

---
+++ precache-manifest.fc17fbe00c72bf9b9ffd57ed8d6963ae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1e936741a37ea08a2e59cb7c57ce6a",
+    "url": "/cindy/index.html"
+  },
+  {
+    "revision": "138e8401b00b923d9446",
+    "url": "/cindy/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0536f7cb78be22797787",
+    "url": "/cindy/static/css/main.94c27a7b.chunk.css"
+  },
+  {
+    "revision": "138e8401b00b923d9446",
+    "url": "/cindy/static/js/2.6fc8d765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cindy/static/js/2.6fc8d765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0536f7cb78be22797787",
+    "url": "/cindy/static/js/main.fa7f8570.chunk.js"
+  },
+  {
+    "revision": "401a248628f259598851",
+    "url": "/cindy/static/js/runtime-main.4cf4c6dd.js"
+  },
+  {
+    "revision": "6882f1abd94f2c9bd6a07116a7a25414",
+    "url": "/cindy/static/media/border.6882f1ab.png"
+  },
+  {
+    "revision": "25e8c816eb1ddd09ac47b55a24381288",
+    "url": "/cindy/static/media/image1.25e8c816.jpg"
+  },
+  {
+    "revision": "64b26ce64d9cf6acb38d221c0dbcc603",
+    "url": "/cindy/static/media/image2.64b26ce6.jpg"
+  },
+  {
+    "revision": "607232e19bd83ea2dd25dbe20a6cf5cf",
+    "url": "/cindy/static/media/image3.607232e1.jpg"
+  },
+  {
+    "revision": "2d375fbf23d404de1e2595c6cbfb9edd",
+    "url": "/cindy/static/media/image4.2d375fbf.jpg"
+  },
+  {
+    "revision": "3952622912b6af2bac4b7b2134a53a45",
+    "url": "/cindy/static/media/image5.39526229.jpg"
+  },
+  {
+    "revision": "582cf3ba1810d20e0bed6affe6415db1",
+    "url": "/cindy/static/media/image7.582cf3ba.jpg"
+  },
+  {
+    "revision": "7bcb09fbb63534b45452e902128ea152",
+    "url": "/cindy/static/media/letter.7bcb09fb.jpg"
+  }
+]);
